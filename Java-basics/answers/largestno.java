338dac2cfd12975dc15eba23a32df611c371e2be
himanshi-modi <himanshimodi421@gmail.com>
2024-11-30 15:02:47 +0530

    Add files via upload

---
+++ Java-basics/answers/largestno.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class largestno {
+    public static void main(String args[]){
+        Scanner n1=new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int no1=n1.nextInt();
+        Scanner n2=new Scanner(System.in);
+        System.out.println("Enter second number: ");
+        int no2=n2.nextInt();
+        if (no1>no2){
+            System.out.println(no1+" is greater");
+        }else{
+            System.out.println(no1+" is greater");
+        }
+
+    }
+}
