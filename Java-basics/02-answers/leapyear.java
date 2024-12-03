2aff11addaed8d35b6db33ec52e61ac1b53405a4
himanshi-modi <himanshimodi421@gmail.com>
2024-12-03 11:05:14 +0530

    Add files via upload

---
+++ Java-basics/02-answers/leapyear.java
@@ -0,0 +1,12 @@
+
+
+public class leapyear {
+    public static void main(String[] args) {
+        int n=2018;
+        if (n % 4==0){
+            System.out.println(n+" is a leap year");
+        }else{
+            System.out.println(n+ " is not a leap year");
+        }
+    }
+}
